a2eb5ad3f39486b92b57d8c5e9f17129f1b22b9c
Jiang Yuhao <1279890372@qq.com>
2023-05-06 22:25:07 +0800

    第二次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,7 +1,6 @@
 # 最终在main函数中传入一个维度为6的numpy数组，输出预测值
 
 import os
-from sklearn.linear_model import Ridge
 
 try:
     import numpy as np
@@ -9,16 +8,13 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
-def ridge(X,Y,data):
+def ridge(x,y,data):
+    loss = np.dot(np.linalg.inv(np.dot(x.T,x)+0.5*np.identity(6,dtype=int)),np.dot(x.T,y))
+    return data @ loss
     
-    ridge_reg=Ridge(alpha=0.4, solver='sag')
-    ridge_reg.fit(X,Y)
-    return ridge_reg.predict(data)
-    
-def lasso(X,Y,data):
-    lasso_reg=Lasso(alpha=0.01, max_iter=30000)
-    lasso_reg.fit(X,Y)
-    return lasso_reg.predict(data)
+def lasso(x,y,data):
+    loss = np.dot(np.linalg.inv(np.dot(x.T,x)),np.dot(x.T,y)+0.5*np.ones(6))
+    return loss @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
